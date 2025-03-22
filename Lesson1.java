580b962127c8d86a6311f3bf54f40e098379adbf
KirillUvarov <kirill@gmail.com>
2025-03-22 19:24:12 +0300

    1

--- Lesson1.java
+++ Lesson1.java
@@ -1,15 +1,15 @@
 public class Lesson1 {
+//    public static void main(String[] args) {
+//        System.out.println("Амиго очень умный");
+//    }
+
+//    public static void main(String[] args) {
+//        System.out.print("Я думаю, ");
+//        System.out.print("быть программистом ");
+//        System.out.println("круто");
+//    }
+
     public static void main(String[] args) {
-        System.out.println("Амиго очень умный");
-    }
-
-    public static void main2(String[] args) {
-        System.out.print("Я думаю, ");
-        System.out.print("быть программистом ");
-        System.out.println("круто");
-    }
-
-    public static void main3(String[] args) {
         String name;
         int age;
         String city;
