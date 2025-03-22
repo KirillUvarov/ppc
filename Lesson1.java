339e874b3addf5ae3fce9a88f0d9911c50cf19e1
KirillUvarov <kirill@gmail.com>
2025-03-22 19:35:05 +0300

    3

--- Lesson1.java
+++ Lesson1.java
@@ -1,6 +1,8 @@
+import java.sql.SQLOutput;
+
 public class Lesson1 {
 //    public static void main(String[] args) {
-//        System.out.println("Амиго очень умный");
+//        System.out.println("Амиго очень умный"); // Если наприсать sout, то IDEA сама создаст конструкцию
 //    }
 
 //    public static void main(String[] args) {
@@ -9,10 +11,29 @@ public class Lesson1 {
 //        System.out.println("круто");
 //    }
 
+//    public static void main(String[] args) {
+//        String name;
+//        int age;
+//        String city;
+//    }
+
+//    public static void main(String[] args) { //если написать main, то IDEA сама создаст необходимую конструкцию
+//        int a, b;
+//        a = 123;
+//        b = 321;
+//        System.out.println(a);
+//        System.out.println(b);
+//    }
+
+//    public static void main(String[] args) {
+//        int a = 3126;
+//        int friend = 8;
+//        int AgeFriend = a - friend;
+//        System.out.println(AgeFriend);
+//    }
+
     public static void main(String[] args) {
-        String name;
-        int age;
-        String city;
+
     }
 
 
